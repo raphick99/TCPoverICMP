876f73c0e97616ca931f9da0a751d276cebc6968
Raphael Ickovics <raphealickovics@gmail.com>
2021-09-09 00:40:45 +0300

    some refractoring to combine code

--- forwarder/forwarder.py
+++ forwarder/forwarder.py
@@ -1,47 +1,15 @@
 import asyncio
 import logging
 
-from tcp import server, client_manager
-from icmp import icmp_socket, icmp_packet
+from core import queue_manager
+from tcp import server
 
 
 log = logging.getLogger(__name__)
 
 
-class Forwarder:
+class Forwarder(queue_manager.QueueManager):
     def __init__(self, host, port):
-        self.incoming_tcp_connections = asyncio.Queue(maxsize=1000)
-        self.incoming_from_icmp_channel = asyncio.Queue(maxsize=1000)
-        self.incoming_from_tcp_channel = asyncio.Queue(maxsize=1000)
-        self.icmp_socket = icmp_socket.ICMPSocket(
-            self.incoming_from_icmp_channel, '127.0.0.1'  # TODO have the endpoint be configurable
-        )
+        super(Forwarder, self).__init__()
         self.tcp_server = server.Server(host, port, self.incoming_tcp_connections)
-        self.client_manager = client_manager.ClientManager(self.incoming_tcp_connections, self.incoming_from_tcp_channel)
-
-    async def run(self):
-        await asyncio.gather(
-            asyncio.create_task(self.handle_incoming_from_icmp_channel()),
-            asyncio.create_task(self.handle_incoming_from_tcp_channel()),
-            asyncio.create_task(self.icmp_socket.wait_for_incoming_packet()),
-            asyncio.create_task(self.tcp_server.serve_forever()),
-            asyncio.create_task(self.client_manager.wait_for_new_connections()),
-        )
-
-    async def handle_incoming_from_icmp_channel(self):
-        while True:
-            new_icmp_packet = await self.incoming_from_icmp_channel.get()
-            log.debug(f'received new packet from icmp. writing to client.')
-            await self.client_manager.write_to_client(new_icmp_packet.payload, new_icmp_packet.identifier)
-
-    async def handle_incoming_from_tcp_channel(self):
-        while True:
-            data, client_id, seq_num = await self.incoming_from_tcp_channel.get()
-            log.debug(f'received from tcp channel: {data}, client_id: {client_id}, seq_num: {seq_num}')
-            new_icmp_packet = icmp_packet.ICMPPacket(
-                type=icmp_packet.ICMPType.EchoRequest,
-                identifier=client_id,
-                sequence_number=seq_num,
-                payload=data,
-            )
-            self.icmp_socket.sendto(new_icmp_packet)
+        self.coroutines_to_run.append(self.tcp_server.serve_forever())
--- tcp/client_manager.py
+++ tcp/client_manager.py
@@ -9,25 +9,49 @@ log = logging.getLogger(__name__)
 
 
 class ClientManager:
-    def __init__(self, new_connections: asyncio.Queue, incoming_tcp_packets: asyncio.Queue):
+    def __init__(
+            self,
+            new_connections: asyncio.Queue,
+            stale_connections: asyncio.Queue,
+            incoming_tcp_packets: asyncio.Queue
+    ):
         self.clients = {}
         self.new_connections = new_connections
+        self.stale_connections = stale_connections
         self.incoming_tcp_packet = incoming_tcp_packets
 
-    async def wait_for_new_connections(self):
+    def client_exists(self, client_id: int):
+        return client_id in self.clients.keys()
+
+    async def wait_for_new_connection(self):
         while True:
             client_id, reader, writer = await self.new_connections.get()
-            await self.add_client(client_id, reader, writer)
+            self.add_client(client_id, reader, writer)
 
-    async def add_client(self, client_id: int, reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
-        self.clients[client_id] = client_session.ClientSession(client_id, reader, writer, self.incoming_tcp_packet)
-        asyncio.create_task(self.clients[client_id].run())
+    async def wait_for_stale_connection(self):
+        while True:
+            client_id = await self.stale_connections.get()
+            self.remove_client(client_id)
+
+    def add_client(self, client_id: int, reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
+        new_client_session = client_session.ClientSession(client_id, reader, writer, self.incoming_tcp_packet)
+        # self.clients[client_id] = client_session.ClientSession(client_id, reader, writer, self.incoming_tcp_packet)
+        new_task = asyncio.create_task(new_client_session.run())
+        self.clients[client_id] = new_client_session, new_task
         log.debug(f'new client running: client_id={client_id}')
+
+    def remove_client(self, client_id: int):
+        log.debug(f'new client running: client_id={client_id}')
+
+        if self.client_exists(client_id):
+            # todo (RI): make nicer, maybe collections.namedtuple
+            self.clients[client_id][0].stop()
+            self.clients[client_id][1].cancel()
 
     async def write_to_client(self, data: bytes, client_id: int):
         try:
-            if client_id in self.clients.keys():
-                await self.clients[client_id].write(data)
+            if self.client_exists(client_id):
+                await self.clients[client_id][0].write(data)
         except ClientClosedConnectionError:
             # TODO Decide what to do in this scenario,
             #  It means the client received a write from the ICMP part, and cannot forward it. Probably return a failure
