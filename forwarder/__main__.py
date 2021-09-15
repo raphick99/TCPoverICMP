52f6b5e36ad0250644ff5977d79317bb244106e9
Raphael Ickovics <raphealickovics@gmail.com>
2021-09-15 08:28:25 +0300

    added cmdline to forwarder

--- forwarder/__main__.py
+++ forwarder/__main__.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+import argparse
 
 import forwarder
 
@@ -8,9 +9,19 @@ logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger(__name__)
 
 
-async def main(host: str, port: int):
-    await forwarder.Forwarder(host, port, '192.168.23.153', 8080).run()
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('proxy_ip', help='IP address of the proxy server')
+    parser.add_argument('listening_port', type=int, help='Port on which the forwarder will listen on')
+    parser.add_argument('destination_ip', help='IP address to forward to')
+    parser.add_argument('destination_port', help='port to forward to')
+    return parser.parse_args()
+
+
+async def main():
+    args = parse_args()
+    await forwarder.Forwarder(args.proxy_ip, args.listening_port, args.destination_ip, args.destination_port).run()
 
 
 if __name__ == '__main__':
-    asyncio.run(main('0.0.0.0', 13337))
+    asyncio.run(main())
--- forwarder/forwarder.py
+++ forwarder/forwarder.py
@@ -10,13 +10,15 @@ log = logging.getLogger(__name__)
 
 
 class Forwarder(tunnel_endpoint.TunnelEndpoint):
-    def __init__(self, other_endpoint, host, port, destination_host, destination_port):
+    LOCALHOST = ''
+
+    def __init__(self, other_endpoint, port, destination_host, destination_port):
         super(Forwarder, self).__init__(other_endpoint)
         log.info(f'forwarding to {destination_host}:{destination_port}')
         self.destination_host = destination_host
         self.destination_port = destination_port
         self.incoming_tcp_connections = asyncio.Queue()
-        self.tcp_server = server.Server(host, port, self.incoming_tcp_connections)
+        self.tcp_server = server.Server(self.LOCALHOST, port, self.incoming_tcp_connections)
         self.coroutines_to_run.append(self.tcp_server.serve_forever())
         self.coroutines_to_run.append(self.wait_for_new_connection())
 
